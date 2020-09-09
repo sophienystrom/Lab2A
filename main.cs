8b270be1fa194025bca49ee14f3f49a403ee02c0
snystrom <sophiekarennystrom@gmail.com>
2020-09-09 19:35:31 +0000

    Fixed wording to match professor's output example

--- main.cs
+++ main.cs
@@ -3,11 +3,11 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
 
-    Console.WriteLine ("Enter the name of item(s)");
+    Console.WriteLine ("Enter the name of item");
     var name = Console.ReadLine ();
-    Console.WriteLine ("Enter the quantity of item(s)");
+    Console.WriteLine ("Enter the quantity");
     var t = Console.ReadLine ();
-    Console.WriteLine ("Enter the price of item");
+    Console.WriteLine ("Enter the price");
     var y = Console.ReadLine ();
     var t1 = Convert.ToDouble(t);
     var y1 = Convert.ToDouble(y);
