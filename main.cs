9f1ef64c65114e3a8603ec6d573faf9f2f6d5d3d
snystrom <sophiekarennystrom@gmail.com>
2020-09-09 19:33:38 +0000

    Fixed code to work for non-integer numbers, code is fully working now :)

--- main.cs
+++ main.cs
@@ -3,16 +3,17 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
 
-    Console.WriteLine ("Enter the name of item");
+    Console.WriteLine ("Enter the name of item(s)");
     var name = Console.ReadLine ();
-    Console.WriteLine ("Enter the quantity of item");
+    Console.WriteLine ("Enter the quantity of item(s)");
     var t = Console.ReadLine ();
     Console.WriteLine ("Enter the price of item");
     var y = Console.ReadLine ();
-   var t1 = Convert.ToInt32(t);
-   var y1 = Convert.ToInt32(y);
-   Console.WriteLine 
-   int r = (t1 * y1);
+    var t1 = Convert.ToDouble(t);
+    var y1 = Convert.ToDouble(y);
+    var r = (t1 * y1); /* variable r will be the variable assigned to total price instead of (t1 * y1) for organizational reasons */
+    Console.WriteLine ("The total price of " + name + " = " + "$" + r);
+   
     
     
     
