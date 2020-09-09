e3fb74ab1d9909684590399d02d3d2892f1160e1
snystrom <sophiekarennystrom@gmail.com>
2020-09-09 18:53:33 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+    Console.WriteLine ("Enter the name of item");
+    var name = Console.ReadLine ();
+    Console.WriteLine ("Enter the quantity of item");
+    var t = Console.ReadLine ();
+    Console.WriteLine ("Enter the price of item");
+    var y = Console.ReadLine ();
+   var t1 = Convert.ToInt32(t);
+   var y1 = Convert.ToInt32(y);
+   Console.WriteLine 
+   int r = (t1 * y1);
+    
+    
+    
+
+  }
+}
